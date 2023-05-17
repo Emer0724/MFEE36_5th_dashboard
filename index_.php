85233ac21f39a27f4c2b955be556e6e885b544fc
leeting110213 <130536437+leeting110213@users.noreply.github.com>
2023-05-17 14:41:45 +0800

    Delete index_.php

--- index_.php
+++
@@ -1,8 +0,0 @@
-<?php include './parts/connect_db.php' ?>
-<?php include './parts/html-head.php' ?>
-<?php include './parts/aside.php' ?>
-<?php include './parts/navbar.php' ?>
-
-
-<?php include './parts/scripts.php' ?>
-<?php include './parts/html-foot.php' ?>
