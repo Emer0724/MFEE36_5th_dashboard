7fa15768d09e6e8b676fcaa93f956bd702a6e6cf
Helen <67916846+hairin0@users.noreply.github.com>
2023-05-19 10:17:45 +0800

    add f-index.php

---
+++ bookstore/forum-tt/f-index.php
@@ -0,0 +1,33 @@
+<?php
+# MVC
+require './parts/connection.php';
+?>
+<?php
+$title_1 = '論壇';
+$title_2 = '論壇管理';
+
+?>
+<?php include('./parts/html-head.php') ?>
+
+<?php include('./parts/aside.php') ?>
+
+<?php include('./parts/navbar.php') ?>
+
+<div class="card-body d-flex justify-content-evenly align-items-center text-center" style="padding:80px;">
+
+
+        <a href="./post.php" class="btn btn-secondary fs-3 d-flex justify-content-center align-items-center" style="width: 200px; height: 200px; ">貼文管理</a>
+
+
+        <a href="./category.php" class="btn btn-secondary fs-3 d-flex justify-content-center align-items-center" style="width: 200px;height: 200px;">類別管理</a>
+
+
+        <a href="./comment.php" class="btn btn-secondary fs-3 d-flex justify-content-center align-items-center" style="width: 200px;height: 200px;">留言管理</a>
+</div>
+
+
+
+
+<?php include './parts/scripts.php' ?>
+
+<?php include './parts/html-foot.php' ?>
